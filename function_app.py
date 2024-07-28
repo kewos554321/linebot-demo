e318b6336371d4419cff385c4e9a6281876d2dc0
Jay Wang <jay.wang@wpgholdings.com>
2024-07-28 11:53:41 +0800

    main: add github workflow

--- function_app.py
+++ function_app.py
@@ -6,7 +6,7 @@ import logging
 app = func.FunctionApp()
 
 @app.route(route="linebot-housedreamer", auth_level=func.AuthLevel.ANONYMOUS)
-def lineWebhook(req: func.HttpRequest) -> func.HttpResponse:
+def linebotHouseDreamer(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
@@ -20,6 +20,27 @@ def lineWebhook(req: func.HttpRequest) -> func.HttpResponse:
 
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully. Yeah~")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+    
+@app.route(route="linebot-housedreamer-email", auth_level=func.AuthLevel.ANONYMOUS)
+def linebotHouseDreamerByEmail(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    email = req.params.get('email')
+    if not email:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            email = req_body.get('email')
+
+    if email:
+        return func.HttpResponse(f"Hello, this is my email={email}")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
